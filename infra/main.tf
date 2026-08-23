4a3c919e60974a4c55cd472e7aaafbba91e3e84f
perpouh <perpouh@gmail.com>
2026-08-23 11:14:29 +0900

    zipここにあるのおかしい

--- infra/main.tf
+++ infra/main.tf
@@ -6,5 +6,13 @@ terraform {
   }
 }
 
-resource "null_resource" "test" {}
-
+resource "aws_lambda_function" "tf-gha-test" {
+  function_name = "tf-gha-test"
+  role          = "arn:aws:iam::050478186852:role/service-role/tf-gha-test-role-iv4rs4b1"
+  filename      = "../layer.zip"
+  handler       = "index.handler"
+  runtime       = "nodejs24.x"
+  layers = [
+    "arn:aws:lambda:ap-northeast-1:050478186852:layer:tf-gha-test-layer:3",
+  ]
+}
